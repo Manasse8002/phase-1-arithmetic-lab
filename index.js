401b229f1ac0db4412cfaa55a66beded9fcd2a59
git_manasse8002 <manassemburu@gmail.com>
2022-12-07 12:28:57 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2 ;
+
+const multiply = (num1 * num2);
+
+let random = 0 ;
+++random;
+
+let num3 = 10;
+let num4 = 6;
+let sum =(num3 + num4); 
+
+ let mod = sum %= 6;
+
+ mod;
+
+let max = Math.max(10,12,14,16,18,20);
+max;
